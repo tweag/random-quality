8de16e08b11f7759f4db7131d7323240a30d0e0d
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-02-17 10:23:46 +0100

    Fix gjrand, add float tests

--- nix/gjrand/default.nix
+++ nix/gjrand/default.nix
@@ -7,18 +7,33 @@ stdenv.mkDerivation {
     sha256 = "1qzh902phgj5204lnmmghmxa08q3jia6hp7jy6ha8iybms0wci98";
   };
 
+  patchPhase = ''
+    patch -p1 < ${./gjrand-4.3.0.0-skip-rda.patch}
+  '';
   buildPhase = ''
     pushd src
-    bash compile
+        bash compile
     popd
 
     pushd testunif/src
-    bash compile
+        sed -i "s:comfmt=\"bin:comfmt=\"$out/testunif-bin:" mcp.c
+        sed -i "s:comfmt=\"bin:comfmt=\"$out/testunif-bin:" pmcp.c
+        bash compile
+    popd
+
+    pushd testfunif/src
+        sed -i "s:comfmt=\"bin:comfmt=\"$out/testfunif-bin:" mcpf.c
+        sed -i "s:comfmt=\"bin:comfmt=\"$out/testfunif-bin:" pmcpf.c
+        bash compile
     popd
   '';
   installPhase = ''
-    mkdir -p $out/bin
-    cp testunif/pmcp $out/bin
+    mkdir -p $out/bin $out/testunif-bin $out/testfunif-bin
+    cp testunif/mcp testunif/pmcp $out/bin
+    cp testunif/bin/* $out/testunif-bin
+
+    cp testfunif/mcpf testfunif/pmcpf testfunif/gen/uni2f $out/bin
+    cp testfunif/bin/* $out/testfunif-bin
   '';
 
   meta = with stdenv.lib; {
